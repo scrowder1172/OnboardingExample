fc8f6cd5876e150c86513588116e31930b71f6c3
Scott Crowder <scrowder@gmail.com>
2024-03-26 09:26:48 -0500

    Initial Commit

---
+++ OnboardingExample/OnboardingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingExampleApp.swift
+//  OnboardingExample
+//
+//  Created by SCOTT CROWDER on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
